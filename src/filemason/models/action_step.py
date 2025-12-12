939a0c1891c3f9b50e9b6c62592bf0443f16a191
Karson Bryant <karson.r.bryant@outlook.com>
2025-12-12 16:46:20 -0600

    refactor(models) migrated action_steps.py to pydantic BaseModel

--- src/filemason/models/action_step.py
+++ src/filemason/models/action_step.py
@@ -1,22 +1,20 @@
 """Domain model for representing a singular action step to be taken"""
 
-from dataclasses import dataclass
 from enum import Enum
 from pathlib import Path
+from pydantic import BaseModel, ConfigDict
 
 
-class Action(Enum):
+class Action(str, Enum):
     """Enumeration of possible actions an ActionStep can perform."""
 
     move = "MOVE"
     mkdir = "MKDIR"
 
-    def __str__(self):
-        return self.value
 
 
-@dataclass(frozen=True)
-class ActionStep:
+
+class ActionStep(BaseModel):
     """
     Domain model for a singular action step to be taken by the application.
 
@@ -24,10 +22,12 @@ class ActionStep:
         id: The hashed ID of the FileItem
         action: The type of action to perform (move, mkdir, or rename).
         source: The source path relevant to the operation. May be None for mkdir.
-        destination: The destination path relevant to the operation. May be None for rename.
+        destination: The destination path relevant to the operation.
     """
 
-    file_id: str | None
+    model_config = ConfigDict(frozen=True, extra='forbid')
+
+    file_id: str | None = None
     action: Action
-    source: Path | None
+    source: Path | None = None
     destination: Path
